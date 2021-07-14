eed79f92c97f270e746fe8095adbd32ba8606eef
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-14 16:33:41 -0700

    add method to loop through word and find palindrome

---
+++ ruby_script.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+require ('palindromes.rb')
+puts "give us a word and we'll tell you if it's a palindrome"
+word = get.chomp
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -6,4 +6,8 @@ describe ('#palindromes') do
     word = Word.new('baaab')
     expect(word.is_palindrome).to(eq(true))
   end
+  it("returns true if a word is the same backwards as forwards") do
+    word = Word.new('rot')
+    expect(word.is_palindrome).to(eq(true))
+  end
 end
