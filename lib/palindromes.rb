abf6f16ace2e0d0bc5f2c75e28421ce5cc7e5cd7
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-14 16:08:06 -0700

    add Palindrome module

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -1,1 +1,22 @@
 require ('pry')
+
+module Palindrome
+  def is_palindrome()
+    @word_array = @word.chars
+    @array_length = @word_array.length
+    if @array_length > 2
+      if @word_array[0] === @word_array[@word_array.length-1]
+        return true
+      end
+    end
+  end
+end
+
+class Word
+  include Palindrome
+    def initialize(word)
+      @word = word
+    end
+  
+
+end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -1,2 +1,9 @@
 require ('rspec')
 require ('palindromes')
+
+describe ('#palindromes') do
+  it("returns true if the first and last letter of a word is the same") do
+    word = Word.new('baaab')
+    expect(word.is_palindrome).to(eq(true))
+  end
+end
